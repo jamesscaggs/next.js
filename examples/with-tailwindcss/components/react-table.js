75ce9bee5a83c91451824a15086aa00056b37704
jamesscaggs <james.scaggs@gmail.com>
2020-12-02 17:25:11 -0600

    add filtering to table

--- examples/with-tailwindcss/components/react-table.js
+++ examples/with-tailwindcss/components/react-table.js
@@ -1,54 +1,23 @@
 import React from 'react'
 import { useTable, useFilters } from 'react-table'
 
-// import { data } from '../data'
+import ColumnFilter from './columnFilter'
+
+import { COLUMNS } from './columns'
+import { DATA } from './data'
 
 export default function ReactTable() {
-  const data = React.useMemo(() => [
-    {
-      _id: { $oid: '5f760d660ab6be0162002176' },
-      firstName: 'Cynthie',
-      lastName: 'Kassel',
-      email: 'ckassel6@bloglovin.com',
-      password: 'hRTAuw',
-      employer: 'Vitamin Water',
-      title: 'Full Stack Developer',
-      languages: 'React',
-    },
-    {
-      _id: { $oid: '5f760d660ab6be016200217c' },
-      firstName: 'Susanne',
-      lastName: 'Guppey',
-      email: 'sguppeyc@purevolume.com',
-      password: '6vspqYjsJ',
-      employer: 'UnitedHealth Group',
-      title: 'Front End Developer',
-      languages: 'React',
-    },
-  ])
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'First name',
-        accessor: 'firstName', // accessor is the "key" in the data
-      },
-      {
-        Header: 'Last name',
-        accessor: 'lastName',
-      },
-      {
-        Header: 'Employer',
-        accessor: 'employer',
-      },
-      {
-        Header: 'Title',
-        accessor: 'title',
-      },
-    ],
+  const columns = React.useMemo(() => COLUMNS, [])
+  const data = React.useMemo(() => DATA, [])
+
+  const defaultColumn = React.useMemo(
+    () => ({
+      Filter: ColumnFilter,
+    }),
     []
   )
 
-  const tableInstance = useTable({ columns, data }, useFilters)
+  const tableInstance = useTable({ columns, data, defaultColumn }, useFilters)
 
   const {
     getTableProps,
@@ -56,6 +25,7 @@ export default function ReactTable() {
     headerGroups,
     rows,
     prepareRow,
+    state,
   } = tableInstance
 
   return (
@@ -74,6 +44,9 @@ export default function ReactTable() {
                     className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
                     {column.render('Header')}
+                    <div>
+                      {column.canFilter ? column.render('Filter') : null}
+                    </div>
                   </th>
                 ))}
               </tr>
